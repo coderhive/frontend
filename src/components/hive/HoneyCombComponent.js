94effebf60f968d8e80547c6480e2bac79e4a2fa
Melisa Im <melisaevelynim@gmail.com>
2017-12-03 17:18:26 -0800

    Add HoneyCombComponent file

---
+++ src/components/hive/HoneyCombComponent.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HoneycombComponent() {
+	return <div>hi</div>;
+}
---
+++ src/components/hive/HoneyCombComponent.story.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import HoneyCombComponent from "./HoneyCombComponent";
+
+storiesOf("HoneyCombComponent", module).add("Happy Path", () => <HoneyCombComponent />);
