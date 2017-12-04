26c99bc491ca97864d7ea734aa5ee228b6afcf06
Melisa Im <melisaevelynim@gmail.com>
2017-12-03 17:10:37 -0800

    Add Bearer Token to graphql

--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import ApolloClient from "apollo-client";
-import { HttpLink } from "apollo-link-http";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
 import "./index.css";
 
+const httpLink = createHttpLink({
+	uri: "http://localhost:3000/graphql"
+});
+
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem("token");
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : null
+		}
+	};
+});
+
 const client = new ApolloClient({
-	link: new HttpLink({ uri: "http://localhost:3000/graphql" }),
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache()
 });
 
