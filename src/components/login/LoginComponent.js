707c7f4af19241473d9b494f53c9974fdb05449b
Melisa Im <melisaevelynim@gmail.com>
2017-12-03 17:22:23 -0800

    Add basic login form

--- src/components/login/LoginComponent.js
+++ src/components/login/LoginComponent.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { PureComponent } from "react";
+import { Button, Form } from "semantic-ui-react";
 
-export default function LoginComponent() {
-	return <div>hi</div>;
+export default class LoginComponent extends PureComponent {
+	handleSubmit = () => {};
+
+	render() {
+		return (
+			<Form unstackable onSubmit={this.handleSubmit}>
+				<Form.Group widths={1}>
+					<Form.Input label="Email" placeholder="Email" />
+					<Form.Input label="Password" placeholder="Password" />
+				</Form.Group>
+				<Button type="submit">Login</Button>
+			</Form>
+		);
+	}
 }
