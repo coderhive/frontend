d7a94b2cd8ec86e4877ebeeffa4a1f5e254d5deb
Melisa Im <melisaevelynim@gmail.com>
2017-12-03 17:20:33 -0800

    Add HoneyCombComponentGrid file

---
+++ src/components/hive/HoneyCombComponentGrid.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HoneycombComponentGrid() {
+	return <div>hi</div>;
+}
---
+++ src/components/hive/HoneyCombComponentGrid.story.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import HoneyCombComponentGrid from "./HoneyCombComponentGrid";
+
+storiesOf("HoneyCombComponentGrid", module).add("Happy Path", () => <HoneyCombComponentGrid />);
