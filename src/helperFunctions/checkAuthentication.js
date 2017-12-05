bd0831f44aec4d2c93dc5174e0bd0efda41cfaa7
Melisa Im <melisaevelynim@gmail.com>
2017-12-04 16:37:57 -0800

    Add user if token is available w/ helperFunction, remove token if it is expired

---
+++ src/helperFunctions/checkAuthentication.js
@@ -0,0 +1,28 @@
+import jwtDecode from "jwt-decode";
+
+export default async function checkAuthentication({ baseUrl }) {
+	try {
+		const token = localStorage.getItem("token");
+
+		if (!token) {
+			localStorage.removeItem("token");
+			return null;
+		}
+
+		const { sub: id, exp: expiration } = jwtDecode(token);
+
+		if (expiration > Date.now()) {
+			console.log("expired token");
+			localStorage.removeItem("token");
+			return null;
+		}
+
+		const user = await this.props.oneUser({ variables: { id } });
+		return { user, token };
+	} catch (error) {
+		console.log(error);
+
+		localStorage.removeItem("token");
+		return null;
+	}
+}
