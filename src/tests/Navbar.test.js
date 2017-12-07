3765bfd16e939d67f7cb990fb268c837d26935ec
ChuckHagy <chuckhagy@gmail.com>
2017-12-07 11:29:36 -0800

    testing navbar component

--- src/tests/Navbar.test.js
+++ src/tests/Navbar.test.js
@@ -7,6 +7,8 @@ import React from 'react';
 import NavBar from '../components/nav/Navbar';
 import {shallow, mount} from 'enzyme';
 
+const submitFn = jest.fn();
+
 describe('Tests the navbar component', () => {
     const wrapper = shallow(
         <NavBar
@@ -14,9 +16,38 @@ describe('Tests the navbar component', () => {
             user={[]}
         />
     );
+
+
     it('should have three menu items - and no other direct children', () => {
-        // console.log('>>>>>>>>>>', wrapper.children(), '<<<<<<<<<<')
         expect(wrapper.find('MenuItem')).toHaveLength(3);
         expect(wrapper.children()).toHaveLength(3);
-    })
+    });
+
+    it('should have a first child with a FUNCTION type and ONE child of its own', () => {
+        expect(wrapper.childAt(0)).toHaveLength(1);
+        expect(typeof wrapper.childAt(0).type()).toBe('function');
+    });
+
+    it('should have a an IMAGE of size TINY with a 20px top margin', () => {
+        expect(wrapper.childAt(0).childAt(0).prop('as')).toEqual('img');
+        expect(wrapper.childAt(0).childAt(0).prop('size')).toEqual('tiny');
+        expect(wrapper.childAt(0).childAt(0).prop('style')['marginTop']).toEqual('20px');
+    });
+
+    it('should have a second child with a FUNCTION type and ONE child of its own', () => {
+        expect(wrapper.childAt(1)).toHaveLength(1);
+        expect(typeof wrapper.childAt(1).type()).toBe('function');
+    });
+
+    it('should have a third child with a FUNCTION type and ONE child of its own', () => {
+        expect(wrapper.childAt(2)).toHaveLength(1);
+        expect(typeof wrapper.childAt(2).type()).toBe('function');
+    });
+
+    it('should have two options under login - LOGIN and SIGN UP - both of which are FUNCTIONS', () => {
+        expect(wrapper.childAt(2).childAt(0).children().children()).toHaveLength(2);
+        expect(typeof wrapper.childAt(2).childAt(0).children().childAt(0).type()).toBe('function');
+        expect(typeof wrapper.childAt(2).childAt(0).children().childAt(0).type()).toBe('function');
+    });
+
 });
