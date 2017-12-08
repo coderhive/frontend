adae8faa3ab634bc4aec61c440e72f871a6e46dd
ChuckHagy <chuckhagy@gmail.com>
2017-12-08 12:56:52 -0800

    editor in container now

--- src/App.js
+++ src/App.js
@@ -1,79 +1,96 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import HiveContainer from "./graphql/HiveContainer";
 import SignupContainer from "./graphql/SignupContainer";
+import EditorContainer from "./graphql/EditorContainer";
 import login from "./helperFunctions/login";
 import jwtDecode from "jwt-decode";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			authenticatedId: this.props.authenticatedId
-		};
-	}
-	handleLogin = async ({ email, password }) => {
-		let { token } = await login({ email, password }, "http://localhost:3000");
-		if (token) {
-			localStorage.setItem("token", token);
-			let { sub: authenticatedId } = jwtDecode(token);
-			this.setState({ authenticatedId: parseInt(authenticatedId, 10) });
-		}
-		return;
-	};
+    constructor(props) {
+        super(props);
+        this.state = {
+            authenticatedId: this.props.authenticatedId
+        };
+    }
 
-	handleLogOut = () => {
-		localStorage.removeItem("token");
-		this.setState({ authenticatedId: null });
-	};
+    handleLogin = async ({email, password}) => {
+        let {token} = await login({email, password}, "http://localhost:3000");
+        if (token) {
+            localStorage.setItem("token", token);
+            let {sub: authenticatedId} = jwtDecode(token);
+            this.setState({authenticatedId: parseInt(authenticatedId, 10)});
+        }
+        return;
+    };
 
-	render() {
-		return (
-			<Router>
-				<div>
-					<Route
-						exact
-						path="/"
-						render={() => {
-							return (
-								<HiveContainer
-									handleLogin={this.handleLogin}
-									handleLogOut={this.handleLogOut}
-									authenticatedId={this.state.authenticatedId}
-								/>
-							);
-						}}
-					/>
-					<Route
-						exact
-						path="/hive"
-						render={() => {
-							return (
-								<HiveContainer
-									handleLogin={this.handleLogin}
-									handleLogOut={this.handleLogOut}
-									authenticatedId={this.state.authenticatedId}
-								/>
-							);
-						}}
-					/>
-					<Route
-						exact
-						path="/signup"
-						render={() => {
-							return (
-								<SignupContainer
-									authenticatedId={this.state.authenticatedId}
-									handleLogin={this.handleLogin}
-									handleLogOut={this.handleLogOut}
-								/>
-							);
-						}}
-					/>
-				</div>
-			</Router>
-		);
-	}
+    handleLogOut = () => {
+        localStorage.removeItem("token");
+        this.setState({authenticatedId: null});
+    };
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route
+                        exact
+                        path="/"
+                        render={() => {
+                            return (
+                                <HiveContainer
+                                    handleLogin={this.handleLogin}
+                                    handleLogOut={this.handleLogOut}
+                                    authenticatedId={this.state.authenticatedId}
+                                />
+                            );
+                        }}
+                    />
+                    <Route
+                        exact
+                        path="/hive"
+                        render={() => {
+                            return (
+                                <HiveContainer
+                                    handleLogin={this.handleLogin}
+                                    handleLogOut={this.handleLogOut}
+                                    authenticatedId={this.state.authenticatedId}
+                                />
+                            );
+                        }}
+                    />
+                    <Route
+                        exact
+                        path="/components/:componentId"
+                        render={({match}) => {
+                            const id = parseInt(match.params.componentId);
+                            return (
+                                <EditorContainer
+                                    componentId={id}
+                                    handleLogin={this.handleLogin}
+                                    handleLogOut={this.handleLogOut}
+                                    authenticatedId={this.state.authenticatedId}
+                                />
+                            );
+                        }}
+                    />
+                    <Route
+                        exact
+                        path="/signup"
+                        render={() => {
+                            return (
+                                <SignupContainer
+                                    authenticatedId={this.state.authenticatedId}
+                                    handleLogin={this.handleLogin}
+                                    handleLogOut={this.handleLogOut}
+                                />
+                            );
+                        }}
+                    />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/components/editor/EditorPanes.js
+++ src/components/editor/EditorPanes.js
@@ -1,4 +1,8 @@
 import React, {PureComponent} from "react";
+import {Button} from 'semantic-ui-react'
+import NavBar from '../../graphql/NavbarContainer'
+
+const moment = require('moment');
 
 export default class EditorPage extends PureComponent {
     constructor(props) {
@@ -6,8 +10,7 @@ export default class EditorPage extends PureComponent {
 
         this.state = {
             panel1Collapsed: false,
-            panel3Collapsed: true,
-            panel4Collapsed: true,
+            panel3Collapsed: false,
         };
     }
 
@@ -17,70 +20,99 @@ export default class EditorPage extends PureComponent {
         if (panelNumber === 4) this.setState({panel4Collapsed: !this.state.panel4Collapsed});
     };
 
+    
+
     render() {
+        if (this.props.data.loading) return (<p>LOADING...</p>);
         return (
-            <div className="editorContainer">
-                <div className={this.state.panel1Collapsed ? "panelsVerticalCollapsed" : "panelsVertical"}>
-                    <div className="toolbarBg">
-                        <div className="editorCollapseButton" onClick={() => this.handleToggle(1)}>
+            <div>
+                <NavBar />
+                <div className="editorContainer">
+                    <div className="editorPaneContainer">
+                        <div className={this.state.panel1Collapsed ? "panelsVerticalCollapsed" : "panelsVertical"}>
+                            <div className="toolbarBg">
+                                <div className="editorCollapseButton" onClick={() => this.handleToggle(1)}>
+                                    {this.state.panel1Collapsed ?
+                                        <i className="plus square outline icon" size="mini"></i> :
+                                        <i className="minus square outline icon" size="mini"></i>}
+                                </div>
+                            </div>
                             {this.state.panel1Collapsed ?
-                                <i class="plus square outline icon" size="mini"></i> :
-                                <i class="minus square outline icon" size="mini"></i>}
+                                <div className="textHolderCollapsed">
+                                    <p className="closedText">Editor</p>
+                                </div>
+                                :
+                                <p className="bodyText">Code Editor</p>
+                            }
                         </div>
-                    </div>
-                    {this.state.panel1Collapsed ?
-                        <div className="textHolderCollapsed">
-                            <p className="closedText">Editor</p>
+                        <div className="panelsVertical">
+                            <div className="toolbarBg" style={{"paddingLeft": "8px"}}>
+                                <div className="editorRefreshButton">
+                                    <i className="refresh icon" size="mini"></i>
+                                </div>
+                            </div>
+                            <p className="bodyText">Main Render</p>
                         </div>
-                        :
-                        <p className="bodyText">Editor Panel</p>
-                    }
-                </div>
-                <div className="centerPanelVertical">
-                    <div className="toolbarBg" style={{"paddingLeft": "8px"}}>
-                        <div className="editorRefreshButton">
-                            <i class="refresh icon" size="mini"></i>
-                        </div>
-                    </div>
-                    <p className="bodyText">Main Render Space</p>
-                </div>
-                <div className={this.state.panel3Collapsed ? "panelsVerticalCollapsed" : "panelsVertical"}>
-                    <div className="toolbarBg">
-                        <div className="editorCollapseButton" onClick={() => this.handleToggle(3)}>
+                        <div className={this.state.panel3Collapsed ? "panelsVerticalCollapsed" : "panelsVertical"}>
+                            <div className="toolbarBg">
+                                <div className="editorCollapseButton" onClick={() => this.handleToggle(3)}>
+                                    {this.state.panel3Collapsed ?
+                                        <i className="plus square outline icon" size="mini"></i> :
+                                        <i className="minus square outline icon" size="mini"></i>}
+                                </div>
+                            </div>
                             {this.state.panel3Collapsed ?
-                                <i class="plus square outline icon" size="mini"></i> :
-                                <i class="minus square outline icon" size="mini"></i>}
+                                <div className="textHolderCollapsed">
+                                    <p className="closedText">Styling</p>
+
+                                </div>
+                                :
+                                <p className="bodyText">CSS Panel</p>
+                            }
                         </div>
                     </div>
-                    {this.state.panel3Collapsed ?
-                        <div className="textHolderCollapsed">
-                            <p className="closedText">Tests</p>
-
+                    <div className="detailsBox">
+                        <div className="boxDetail1">
+                            <h1>{this.props.data.oneComponent.title}</h1>
+                            <p>framework: {this.props.data.oneComponent.framework.toUpperCase()}</p>
+                            <p>created: {moment(this.props.data.oneComponent.created_at).format('LLLL')}</p>
+                            <p>updated: {moment(this.props.data.oneComponent.updated_at).format('LLLL')}</p>
                         </div>
-                        :
-                        <p className="bodyText">Testing Panel</p>
-                    }
-                </div>
-                <div className={this.state.panel4Collapsed ? "panelsVerticalCollapsed" : "panelsVertical"}>
-                    <div className="toolbarBg">
-                        <div className="editorCollapseButton" onClick={() => this.handleToggle(4)}>
-                            {this.state.panel4Collapsed ?
-                                <i class="plus square outline icon" size="mini"></i> :
-                                <i class="minus square outline icon" size="mini"></i>}
+                        <div className="boxDetail2">
+                            <p>{this.props.data.oneComponent.tags.length} tags:</p>
+                            {this.props.data.oneComponent.tags.map((thisTag) =>
+                                <div className="tag" key={thisTag.id}>{thisTag.name}</div>)
+                            }
+                            <div className="tag">...</div>
+                            <div style={{marginTop: "20px"}}>
+                                <p>{this.props.data.oneComponent.fans.length} fans:</p>
+                                {this.props.data.oneComponent.fans.map((thisFan) =>
+                                    <div className="fan" key={thisFan.id}>{thisFan.display_name}</div>)
+                                }
+                                <div className="fan">...</div>
+                            </div>
+                        </div>
+                        <div className="boxDetail3">
+                            <Button
+                                color='green'
+                                content='Like'
+                                icon='like outline'
+                                label={{basic: true, color: 'green', pointing: 'left', content: '2,048'}}
+                                style={{borderBottom: "25px"}}
+                            />
+                            <Button
+                                color='black'
+                                content='Dislike'
+                                icon='dislike outline'
+                                label={{basic: true, color: 'black', pointing: 'left', content: '2,048'}}
+                            />
                         </div>
                     </div>
-
-                    {this.state.panel4Collapsed ?
-                        <div className="textHolderCollapsed">
-                            <p className="closedText">Controls</p>
-                        </div>
-                        :
-                        <p className="bodyText">Controls Panel</p>
-                    }
                 </div>
-            </div>
-        )
-    }
+                </div>
 
-}
+                )
+                }
+
+                }
 
