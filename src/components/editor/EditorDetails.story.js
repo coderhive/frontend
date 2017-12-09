45ab6002102095b8139da48a5a7a43f6dcb023d7
ChuckHagy <chuckhagy@gmail.com>
2017-12-09 15:14:29 -0800

    prepping for comment component

--- src/components/editor/EditorDetails.story.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import EditorDetails from "./EditorDetails";
-
-storiesOf("EditorDetails", module).add("Happy Path", () => <EditorDetails />);
