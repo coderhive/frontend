6c7ccc45356259429fa7af3b148665bf44b4fd90
ChuckHagy <chuckhagy@gmail.com>
2017-12-11 14:48:42 -0800

    working top bio in user profile

--- src/components/user/UserProfile.js
+++ src/components/user/UserProfile.js
@@ -1,5 +1,5 @@
 import React, {PureComponent} from "react";
-import {Button} from 'semantic-ui-react'
+import {Loader} from 'semantic-ui-react'
 import NavBar from '../../graphql/NavbarContainer'
 
 const moment = require('moment');
@@ -22,46 +22,116 @@ export default class UserProfile extends PureComponent {
     }
 
     render() {
-        console.log('foo bar')
-        if (this.props.data.loading) return (<p style={{color: "white"}}>LOADING...</p>);
-        if (!this.props.data.oneUserById) return (<p>LOADING USER DATA...</p>);
         return (
-            <div style={{color: "white"}}>
-                <p>{this.props.data.oneUserById.id}</p>
-                <p>{this.props.data.oneUserById.display_name}</p>
-                <p>{this.props.data.oneUserById.summary}</p>
-                <p>{this.props.data.oneUserById.experience}</p>
-                <p>{this.props.data.oneUserById.profile_picture}</p>
-                <p>{this.props.data.oneUserById.created_at}</p>
-
-                {this.props.data.oneUserById.components.map(component =>
-                    <p key={component.id}>{component.title}</p>
-                )}
-
-                {this.props.data.oneUserById.fanOf.map(component =>
-                    <p key={component.id}>{component.title}</p>
-                )}
-
-                {this.props.data.oneUserById.followers.map(follower =>
-                    <p key={follower.id}>{follower.display_name}</p>
-                )}
-
-                {this.props.data.oneUserById.whoIFollow.map(user =>
-                    <p key={user.id}>{user.display_name}</p>
-                )}
-
-                {this.props.data.oneUserById.activities.map(activity =>
-                    <div key={activity.id}>
-                    <p>{activity.type}</p>
-                    <p>{activity.user_id}</p>
-                    <p>{activity.component_id}</p>
-                    <p>{activity.comment ? activity.comment.comment : ' -- '}</p>
+            <div>
+                <NavBar
+                    authenticatedId={this.props.authenticatedId}
+                    user={[this.props.loggedUser]}
+                    onSubmit={this.props.handleLogin}
+                    onLogout={this.props.handleLogOut}
+                />
+                {this.props.data.loading ?
+                    <div className="centeredSpinner">
+                        <Loader
+                            active
+                            size="massive"
+                        />
                     </div>
-                )}
+                    :
+                    <div className="userContainer">
+                        <div>
+                            <div className="profileBioTopBlack">
+                                {/*<h4>User Profile:</h4>*/}
+                                <div className="topBioHolder1">
+                                    <div className="honeyCombComponent" style={styles.container}>
+                                        <div style={
+                                            this.props.data.oneUserById.profile_picture
+                                                ? {backgroundImage: "url(" + this.props.data.oneUserById.profile_picture + ")"}
+                                                : {
+                                                    backgroundImage:
+                                                        "url(https://static.pexels.com/photos/20787/pexels-photo.jpg)"
+                                                }
+                                        }
+                                             className="honeyCombComponent">
+                                            <div className="honeyCombTop"/>
+                                            <div className="honeyCombBottom"/>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div className="topBioHolder2">
+                                    <h1>{this.props.data.oneUserById.display_name}</h1>
+                                    <div style={{display: "flex", justifyContent: "space-between", textAlign: "center"}}>
+                                        <div style={{flexGrow: "1"}}>
+                                            <h3>rank:</h3>
+                                            <h4>{this.props.data.oneUserById.experience}</h4>
+                                        </div>
+                                        <div style={{flexGrow: "1"}}>
+                                            <h3>member since:</h3>
+                                            <h4>{moment(this.props.data.oneUserById.created_at).format('LLLL')}</h4>
+                                        </div>
+                                    </div>
+                                    <h2>about me:</h2>
+                                    <p>{this.props.data.oneUserById.summary}</p>
+                                </div>
+                            </div>
+                            <div>
+                                {console.log(this.props.data.oneUserById.id)}
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+
+
+                                {this.props.data.oneUserById.components.map(component =>
+                                    <p key={component.id}>{component.title}</p>
+                                )}
+
+                                {this.props.data.oneUserById.fanOf.map(component =>
+                                    <p key={component.id}>{component.title}</p>
+                                )}
+
+                                {this.props.data.oneUserById.followers.map(follower =>
+                                    <p key={follower.id}>{follower.display_name}</p>
+                                )}
+
+                                {this.props.data.oneUserById.whoIFollow.map(user =>
+                                    <p key={user.id}>{user.display_name}</p>
+                                )}
+
+                                {this.props.data.oneUserById.activities.map(activity =>
+                                    <div key={activity.id}>
+                                        <p>{activity.type}</p>
+                                        <p>{activity.user_id}</p>
+                                        <p>{activity.component_id}</p>
+                                        <p>{activity.comment ? activity.comment.comment : ' -- '}</p>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                }
             </div>
 
         )
     }
 
 }
+
+let styles = {
+    container: {
+        width: "301px",
+        paddingLeft: "1.3px",
+        paddingRight: "1.3px"
+    }
+};
 
