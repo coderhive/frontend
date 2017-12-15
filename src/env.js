ee18190690702120ea861d4005fc832bb3c836db
Melisa Im <melisaevelynim@gmail.com>
2017-12-15 11:47:16 -0800

    Add aws credentials

--- src/env.js
+++ src/env.js
@@ -1,3 +1,5 @@
 export default {
-	BASE_URL: process.env.REACT_APP_BASE_URL
+	BASE_URL: process.env.REACT_APP_BASE_URL,
+	AWSKEY: process.env.REACT_APP_AWSKEY,
+	AWSSECRET: process.env.REACT_APP_AWSSECRET
 };
